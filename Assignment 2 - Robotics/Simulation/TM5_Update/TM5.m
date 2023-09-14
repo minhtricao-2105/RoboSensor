5f7d135bb32ebcf611a0f36b547f58d4b027931c
minhtricao-2105 <MinhTri.Cao@student.uts.edu.au>
2023-09-14 12:23:25 +1000

    Finalize TM5 Model

--- Assignment 2 - Robotics/Simulation/TM5_Update/TM5.m
+++ Assignment 2 - Robotics/Simulation/TM5_Update/TM5.m
@@ -1,41 +1,132 @@
-classdef TM5 < BaseClass
-    %% UR3 Universal Robot 3kg payload robot model
-    %
-    % WARNING: This model has been created by UTS students in the subject
-    % 41013. No guarentee is made about the accuracy or correctness of the
-    % of the DH parameters of the accompanying ply files. Do not assume
-    % that this matches the real robot!
+classdef TM5 < handle
+    %% TODO: Write description here:
 
-    properties(Access = public)   
-        plyFileNameStem = 'TM5';
+    %----------------------------------- Properties -------------------------------%
+    properties
+        % Setup the workspace of the robot:
+        workspace
+
+        % Setup the home position (need to calibrate)
+        homeQ = [];
+
+        % Setup the name of the robot:
+        name = ['LinearUR3',datestr(now,'yyyymmddTHHMMSSFFF')];
+
+        % Define the robot model in this class:
+        model;
+
+        % Setup the define transformation:
+        baseTransform = eye(4);
+
+        % The constraint variable of the robot platform:
     end
-    
+
+    %----------------------------------- Properties Hidden -------------------------------%
+    properties (Hidden)
+        axis_h = [];
+        figure_h = [];
+        lightAdded = false;
+        surfaceAdded = false;
+    end
+
     methods
-%% Constructor
+        %% Constructor
         function self = TM5(baseTr)
-            % Create the model of the UR3 robot  
-			self.CreateModel();
+            % Create the model of the UR3 robot
+            self.CreateModel();
             % if there is no special input for base, default value will be applied (=4x4 identity matrix)
-            if nargin < 1			
-				baseTr = eye(4);				
+            if nargin < 1
+                baseTr = eye(4);
             end
             % The base of the UR3 robot is based on the base input
             self.model.base = self.model.base.T * baseTr;
-            
+
             % Plot the UR3 model on simulation environment
-            self.PlotAndColourRobot(); 
+            self.PlotAndColourRobot();
         end
 
-%% CreateModel
+        %% CreateModel
         function CreateModel(self)
             link(1) = Link('d',0.146,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
             link(2) = Link('d',0,'a',-0.329,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
             link(3) = Link('d',0,'a',-0.3115,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-            link(4) = Link('d',0.109,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            % link(5) = Link('d',0.106,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            % link(6) = Link('d',0.1132,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
-             
+            link(4) = Link('d',0.124,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            link(5) = Link('d',0.110,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(6) = Link('d',0.1132,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+
             self.model = SerialLink(link,'name',self.name);
-        end      
+        end
+
+        %% Plot and Colour the Robot:
+        function PlotAndColourRobot(self)
+
+            % Check whenever we define the home position or not:
+            if isempty(self.homeQ)
+                self.homeQ = zeros(1, self.model.n);
+            end
+
+            % Read the ply file:
+            for linkIndex = 0:self.model.n
+                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['TM5Link',num2str(linkIndex),'.ply'],'tri');
+                self.model.faces{linkIndex + 1} = faceData;
+                self.model.points{linkIndex + 1} = vertexData;
+            end
+
+            % Plot the robot:
+            self.figure_h = gcf;
+            self.axis_h = gca;
+            initialSurfaceCount = self.CountTiledFloorSurfaces();
+
+            self.model.plot3d(self.homeQ,'noarrow','workspace',self.workspace);
+
+            % Check if a single surface has been added by plot3d
+            if self.CountTiledFloorSurfaces() - initialSurfaceCount == 1
+                self.surfaceAdded = true;
+            end
+
+            % Check if a light needs to be added
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+                self.lightAdded = true;
+            end
+
+            self.model.delay = 0;
+            handles = findobj('Tag', self.model.name);
+            h = get(handles,'UserData');
+
+            % Colour the robot here:
+            for linkIndex = 0:self.model.n
+                vertexColours = [0.2, 0.5, 0.8]; % Default if no colours in plyData
+                try
+                    vertexColours = [plyData{linkIndex+1}.vertex.red ...
+                        , plyData{linkIndex+1}.vertex.green ...
+                        , plyData{linkIndex+1}.vertex.blue]/255;
+
+                catch ME_1
+                    disp(ME_1);
+                    disp('No vertex colours in plyData');
+                    try
+                        vertexColours = [plyData{linkIndex+1}.face.red ...
+                            , plyData{linkIndex+1}.face.green ...
+                            , plyData{linkIndex+1}.face.blue]/255;
+                    catch ME_1
+                        disp(ME_1);
+                        disp(['Also, no face colours in plyData, so using a default colour: ',num2str(vertexColours)]);
+                    end
+                end
+
+                h.link(linkIndex+1).Children.FaceVertexCData = vertexColours;
+                h.link(linkIndex+1).Children.FaceColor = 'interp';
+            end
+            drawnow();
+        end
+
+        %% CountTiledFloorSurfaces
+        % A way of checking if a base tiled floor surface has been added
+        % that needs deleting
+        function surfaceCount = CountTiledFloorSurfaces(self)
+            surfaceCount = numel(findobj(self.axis_h, 'Type', 'surface', 'Tag', 'tiled_floor'));
+        end
+
     end
 end
