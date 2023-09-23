387fc0e9e89500d1d9bd4294e3ac61a2539f686b
anhtungnguyen <anhtungnguyen1809@gmail.com>
2023-09-24 00:25:26 +1000

    24/09/2023

--- Assignment 2 - Robotics/Simulation/Main/Main.m
+++ Assignment 2 - Robotics/Simulation/Main/Main.m
@@ -46,17 +46,17 @@ product2 = PlaceObjectModified('blueProduct.ply', baseTr2*transl(0.5, 0.25, 0));
 product3 = PlaceObjectModified('greenProduct.ply', baseTr2*transl(0.5, -0.25, 0));
 
 % Make the ttRobot move to the position to pick up the product:
-for i = 1:50
-    ttRobotTM5Mobile.model.base = ttRobotTM5Mobile.model.base.T*transl(0,-0.1,0);
-    pause(0.05);
-end
-
-startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
-endPose = baseTr2*transl(0.5, 0, 0);
-qGuess = ttRobot.model.getpos();
-ttRobot.rmrc(startPose, endPose, qGuess,cam);
-
-startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
-endPose = baseTr2*transl(0.5, 0, 0.3);
-qGuess = ttRobot.model.getpos();
-ttRobot.rmrc(startPose, endPose, qGuess,cam, product1);
+% for i = 1:50
+%     ttRobotTM5Mobile.model.base = ttRobotTM5Mobile.model.base.T*transl(0,-0.1,0);
+%     pause(0.05);
+% end
+% 
+% startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
+% endPose = baseTr2*transl(0.5, 0, 0);
+% qGuess = ttRobot.model.getpos();
+% ttRobot.rmrc(startPose, endPose, qGuess,cam);
+% 
+% startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
+% endPose = baseTr2*transl(0.5, 0, 0.3);
+% qGuess = ttRobot.model.getpos();
+% ttRobot.rmrc(startPose, endPose, qGuess,cam, product1);
