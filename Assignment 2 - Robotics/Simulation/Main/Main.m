247092d443f6b94aaa16c00c5d9fe79ab55b029a
anhtungnguyen <anhtungnguyen1809@gmail.com>
2023-09-25 00:49:00 +1000

    update human collision

--- Assignment 2 - Robotics/Simulation/Main/Main.m
+++ Assignment 2 - Robotics/Simulation/Main/Main.m
@@ -30,8 +30,6 @@ ttRobotTM5Mobile = TM5Mobile(baseTr, qHome);
 ttRobot = TM12(baseTr2, qHome);
 ttArm = Arm(baseTr3);
 ttHuman = Human(baseTr4);
-cam = CameraBaseClass(ttRobot.model.fkine(ttRobot.model.getpos).T);
-cam.camera_h.ishold
 
 hold on
 
@@ -52,17 +50,17 @@ product2 = PlaceObjectModified('blueProduct.ply', baseTr2*transl(0.5, 0.25, 0));
 product3 = PlaceObjectModified('greenProduct.ply', baseTr2*transl(0.5, -0.25, 0));
 
 % Make the ttRobot move to the position to pick up the product:
-% for i = 1:50
-%     ttRobotTM5Mobile.model.base = ttRobotTM5Mobile.model.base.T*transl(0,-0.1,0);
-%     pause(0.05);
-% end
-% 
-% startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
-% endPose = baseTr2*transl(0.5, 0, 0);
-% qGuess = ttRobot.model.getpos();
-% ttRobot.rmrc(startPose, endPose, qGuess,cam);
-% 
-% startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
-% endPose = baseTr2*transl(0.5, 0, 0.3);
-% qGuess = ttRobot.model.getpos();
-% ttRobot.rmrc(startPose, endPose, qGuess,cam, product1);
+for i = 1:50
+    ttRobotTM5Mobile.model.base = ttRobotTM5Mobile.model.base.T*transl(0,-0.1,0);
+    pause(0.05);
+end
+
+startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
+endPose = baseTr2*transl(0.5, 0, 0);
+qGuess = ttRobot.model.getpos();
+ttRobot.rmrc(startPose, endPose, qGuess,ttHuman);
+
+startPose = ttRobot.model.fkine(ttRobot.model.getpos()).T;
+endPose = baseTr2*transl(0.5, 0, 0.3);
+qGuess = ttRobot.model.getpos();
+ttRobot.rmrc(startPose, endPose, qGuess,ttHuman, product1);
