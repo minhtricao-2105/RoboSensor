623cb5abd9467e55b092b8d7aed9062fdcf4e032
anhtungnguyen <anhtungnguyen1809@gmail.com>
2023-10-18 00:57:49 +1100

    small test and update

--- Assignment 2 - Robotics/Simulation/Main/testEllipsoid.m
+++ Assignment 2 - Robotics/Simulation/Main/testEllipsoid.m
@@ -4,8 +4,8 @@ clear all;
 close all;
 
 % qHome = [0, -90, -127, -169, 93.6, 0]*pi/180;
-qHome = [0, -pi/2, -2.705, -1.884, pi/2, 0];
-% qHome = [0, -pi/2, -pi/2, -176*pi/180, pi/2, 0];%-pi/2
+% qHome = [0, -pi/2, -2.705, -1.884, pi/2, 0];
+qHome = [0, -pi/2, -pi/2, -pi/2, pi/2, 0];%-pi/2
 % qHome = [0, 0, 0, 0, 0, 0];
 workspace = [-1 1, -1, 1, 0, 1];
 
@@ -13,7 +13,23 @@ workspace = [-1 1, -1, 1, 0, 1];
 ttRobotTM5Mobile = TM5(eye(4), qHome, workspace);
 % robotTM12 = TM12(eye(4), qHome, workspace);
 
+q1 = [-81 -64.8 -121 -90 180 0]*pi/180;
+q_Matrix = jtraj(qHome, q1, 50);
+
+ttRobotTM5Mobile.model.plot(qHome)
+pause(10.0);
 hold on 
+for i=1:50
+    ttRobotTM5Mobile.model.animate(q_Matrix(i,:));
+    pause(0.1);
+end
+% 
+% q2 = [179 -107 -76 -97.2 90 0]*pi/180;
+% q_Matrix = jtraj(q1, q2, 50);
+% for i=1:50
+%     robotTM12.model.animate(q_Matrix(i,:));
+%     pause(0.1);
+% end
 % Call the ellipsoid function:
 % qDesired = qHome;
 % qDesired(1) = qDesired(1) + pi/2;
@@ -25,6 +41,6 @@ hold on
 %     pause(0.1);
 % end
 
-ttRobotTM5Mobile.CheckSelfCollision
+% ttRobotTM5Mobile.CheckSelfCollision
 % ttRobotTM5Mobile.CreateEllipsoidLinks(true)
 % robotTM12.CreateEllipsoidLinks(true)
